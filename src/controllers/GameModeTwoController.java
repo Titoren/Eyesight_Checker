6647af8f5f2f8f86a8e3fdd53d6556c1d4f433a5
Titoren <cat_siam@mail.ru>
2017-06-19 09:14:18 +0300

    All  window added.

--- src/controllers/GameModeTwoController.java
+++ src/controllers/GameModeTwoController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import eyesightChecker.Main;
 import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 
 import java.io.IOException;
@@ -10,6 +11,9 @@ import java.io.IOException;
  * Created by richard on 19.06.17.
  */
 public class GameModeTwoController extends BasicController {
+    @FXML
+    private GridPane gpGameModeTwo;
+
     @FXML
     private Circle circle;
 
@@ -22,16 +26,24 @@ public class GameModeTwoController extends BasicController {
 //                setText("     Game mode one\ncomplete  successfully!");
 
     }
-
     @FXML
     void moveCircle() {
         circle.setRadius(circle.getRadius() - 0.2);
-        circle.setLayoutX(77);
-        circle.setLayoutY(200);
-        circle.relocate(77,88);
+        circle.setLayoutX(200);
+        circle.setLayoutX(250);
+//        circle.setCenterX(circle.getTranslateX() + circle.getCenterX());
+//        circle.setCenterY(circle.getTranslateY() + circle.getCenterY());
+//        circle.setTranslateX(0);
+//        circle.setTranslateY(0);
+        circle.setCenterY(225);
+        circle.setCenterX(115);
     }
 
     public Circle getCircle() {
         return circle;
+    }
+
+    public GridPane getGpGameModeTwo() {
+        return gpGameModeTwo;
     }
 }
