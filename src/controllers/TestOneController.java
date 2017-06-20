469fddffe7ebd6f5fde14aa911cef6a252e5a883
Titoren <cat_siam@mail.ru>
2017-06-20 17:26:38 +0300

    Test have letters now.

--- src/controllers/TestOneController.java
+++ src/controllers/TestOneController.java
@@ -5,21 +5,29 @@ import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
-import javafx.scene.layout.*;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by richard on 18.06.17.
  */
 public class TestOneController extends BasicController {
-    private List<Rectangle> labels;
+    private List<Label> labels;
+    private int rowQuantity = 9;
+    private int colQuantity = 9;
+    private int index;
 
     @FXML
     private GridPane gpTest;
@@ -33,11 +41,22 @@ public class TestOneController extends BasicController {
     @FXML
     void initialize() {
         labels = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
+        int randomNum;
+
+        for (int i = 0; i < rowQuantity; i++) {
             gpTest.addRow(i);
-            for (int j = 0; j < 4; j++) {
-                gpTest.addColumn(j);
-                addLabel(new Label("R"), j, i);
+            for (int j = 0; j < colQuantity; j++) {
+                randomNum = ThreadLocalRandom.current().nextInt(65, 90 + 1);
+
+                gpTest.addColumn(i);
+                index = ((j + 1) + i * (colQuantity) - 1);
+                labels.add(new Label("" + (char)(randomNum)));//(index + 1)));
+//                labels.get(index).setStyle("-fx-text-fill: blueviolet;");
+
+                labels.get(index).setTextFill(Color.BLACK);
+                labels.get(index).setFont(Font.font("Arial", 15));
+
+                gpTest.add(labels.get(index), j, i);
             }
         }
 
@@ -45,39 +64,30 @@ public class TestOneController extends BasicController {
             if (n instanceof Control) {
                 Control control = (Control) n;
                 control.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                control.setStyle("-fx-background-color: cornsilk; -fx-alignment: center;");
+                control.setStyle("-fx-background-color: white; -fx-alignment: center;");
             }
             if (n instanceof Pane) {
                 Pane pane = (Pane) n;
                 pane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                pane.setStyle("-fx-background-color: cornsilk; -fx-alignment: center;");
+                pane.setStyle("-fx-background-color: white; -fx-alignment: center;");
             }
         }
 
-        gpTest.setStyle("-fx-background-color: #605baf; -fx-padding: 2; -fx-hgap: 2; -fx-vgap: 2;");
+        gpTest.setStyle("-fx-background-color: black; -fx-padding: 2; -fx-hgap: 1; -fx-vgap: 1;");
         // turn layout pixel snapping off on the grid so that grid lines will be an even width.
         gpTest.setSnapToPixel(false);
 
-
+        // TODO: 20.06.17 Add leveling
         ColumnConstraints oneThird = new ColumnConstraints();
-        oneThird.setPercentWidth(100 / 4.0);
+        oneThird.setPercentWidth(100 / (0.0 + colQuantity));
         oneThird.setHalignment(HPos.CENTER);
-        gpTest.getColumnConstraints().addAll(oneThird, oneThird, oneThird);
+        for (int i = 0; i < colQuantity - 1; i++)
+            gpTest.getColumnConstraints().addAll(oneThird);
+
         RowConstraints oneHalf = new RowConstraints();
-        oneHalf.setPercentHeight(100 / 3.0);
+        oneHalf.setPercentHeight(100 / (0.0 + rowQuantity));
         oneHalf.setValignment(VPos.CENTER);
-        gpTest.getRowConstraints().addAll(oneHalf, oneHalf);
-    }
-
-    private void addLabel(Label label, int colIndex, int rowIndex) {
-        gpTest.setHalignment(label, HPos.CENTER);
-        gpTest.setValignment(label, VPos.CENTER);
-        gpTest.add(label, colIndex, rowIndex);
-    }
-
-    private void addButton(Button button, int colIndex, int rowIndex) {
-        gpTest.setHalignment(button, HPos.CENTER);
-        gpTest.setValignment(button, VPos.CENTER);
-        gpTest.add(button, colIndex, rowIndex);
+        for (int i = 0; i < rowQuantity - 1; i++)
+            gpTest.getRowConstraints().addAll(oneHalf);
     }
 }
